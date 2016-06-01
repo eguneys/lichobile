a5d3d7d164073b364d186db0a261d86d10225fd7
eguneys <eguneys@hotmail.com>
2016-06-01 14:54:57 +0300

    Update menuView.js

--- project/src/js/ui/menu/menuView.js
+++ project/src/js/ui/menu/menuView.js
@@ -176,9 +176,7 @@ function renderMenu() {
 
   return (
     <div className="native_scroller">
-      <header className="side_menu_header">
-        {renderHeader(user)}
-      </header>
+      {renderHeader(user)}
       {user && menu.headerOpen() ? renderProfileActions(user) : renderLinks(user)}
     </div>
   );
